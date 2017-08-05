07bef385924e40357697b194738208eba0ac5b3f
Hira Ahmad <hiraahmadch@yahoo.com>
2017-08-06 04:42:33 +0500

    Movie: Added posters

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,6 @@
 class Movie < ActiveRecord::Base
   validates :title, presence: true, length: { maximum: 150 }
+
+  has_many :images, as: :imageable, dependent: :destroy
+  accepts_nested_attributes_for :images, allow_destroy: :true
 end
